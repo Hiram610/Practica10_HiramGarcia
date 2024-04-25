8df2b9607989340ffe89ff0932661fca64361c8f
Hiram610 <hiram.garcia216629@potros.itson.edu.mx>
2024-04-25 13:04:55 -0700

    Proyecto Subido

---
+++ app/src/main/java/mx/edu/potros/practica10_216629/MainActivity.kt
@@ -0,0 +1,71 @@
+package mx.edu.potros.practica10_216629
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import mx.edu.potros.practica10_216629.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        binding.signInAppCompatButton.setOnClickListener {
+            val mEmail = binding.emailEditText.text.toString()
+            val mPassword = binding.passwordEditText.text.toString()
+
+            when{
+                mEmail.isEmpty() || mPassword.isEmpty() ->{
+                    Toast.makeText(baseContext, "Mail o Contraseña incorrecta",
+                        Toast.LENGTH_SHORT).show()
+                }else ->{
+                    SignIn(mEmail,mPassword)
+                }
+            }
+        }
+    }
+
+    private fun SignIn(email: String, password: String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if(task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("TAG", "signInWithEmail:success")
+                    reaload()
+                    //val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("TAG", "signInWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    //updateUI(null)
+                }
+            }
+    }
+    private fun reaload(){
+        val intent = Intent(this, SignInActivity::class.java)
+        this.startActivity(intent)
+    }
+}
---
+++ app/src/main/java/mx/edu/potros/practica10_216629/SignInActivity.kt
@@ -0,0 +1,35 @@
+package mx.edu.potros.practica10_216629
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import mx.edu.potros.practica10_216629.databinding.ActivitySignInBinding
+
+class SignInActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivitySignInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivitySignInBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        auth = Firebase.auth
+
+        binding.welcomeTextView.text = "Hola, bienvenido"
+        binding.catImageView.setImageResource(R.drawable.profile_photo)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
